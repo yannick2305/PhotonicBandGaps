a2b844b6d2748b3de8e94b1a5430334e1236addf
yannick2305 <yannick.debruijn23@gmail.com>
2024-11-18 18:53:29 +0100

    Update Band_1D_Dimer.m

--- Band_1D_Dimer.m
+++ Band_1D_Dimer.m
@@ -1,52 +1,70 @@
-% Spectral Plot for a Dimer
+%{
+    ---------------------------------------------------------
+    Author(s):    [Erik Orvehed HILTUNEN , Yannick DE BRUIJN]
+    Date:         [November 2024]
+    Description:  [Subwavelength Spectral Plot for a 1D Dimer Chain]
+    ---------------------------------------------------------
+%}
 
-delta = 0.1;
-s_1 = 0.8;
-s_2 = 2;
-
-% Define the Bandfunctions
-f_1 = @(x) sqrt(delta) * sqrt((1/s_1 + 1/s_2) + sqrt(1/(s_1)^2 + 1/(s_2)^2 + 2/(s_1*s_2)*cos(x)));
-f_2 = @(x) sqrt(delta) * sqrt((1/s_1 + 1/s_2) - sqrt(1/(s_1)^2 + 1/(s_2)^2 + 2/(s_1*s_2)*cos(x)));
-f_3 = @(x) sqrt(delta) * sqrt(abs((1/s_1 + 1/s_2) - sqrt(abs(1/(s_1)^2 + 1/(s_2)^2 - 2/(s_1*s_2)*cosh(x)))) );
-f_4 = @(x) sqrt(delta) * sqrt(abs((1/s_1 + 1/s_2) + sqrt(abs(1/(s_1)^2 + 1/(s_2)^2 - 2/(s_1*s_2)*cosh(x)))) );
-f_5 = @(x) sqrt(delta) * sqrt(abs((1/s_1 + 1/s_2) + sqrt(abs(1/(s_1)^2 + 1/(s_2)^2 + 2/(s_1*s_2)*cosh(x)))) );
-
-% Define the range for x
-beta_bound = acosh((1/2) * (s_2^2 + s_1^2)/(s_1*s_2));
-x_range = -pi:0.08:pi;
-beta_range = -beta_bound:0.0098:beta_bound+0.001 ;
-beta_range_2 = -4:0.08:4;
-
-% Calculate the values of the function for each point in the grid
-y_values_1 = real(f_1(x_range));
-y_values_2 = real(f_2(x_range));
-y_values_3 = real(f_3(beta_range));
-y_values_4 = real(f_4(beta_range));
-y_values_5 = real(f_5(beta_range_2));
+% Objective:
+%   Generate a spectral plot for a dimer resonator chain. 
+%   A complete derivation of the formula used in this implementation 
+%   can be found in arXiv:2409.14537.
 
 
-% Create the initial plot for the functions
-figure;
-lw = 4.5; % Linewidth
+% --- Set the parameters ---
+    delta = 0.1;
+    s_1 = 0.8;
+    s_2 = 2;
 
-plot(x_range, y_values_1, 'k', 'LineWidth', lw);
-hold on;
-pbaspect([8 6 1]); % Set the ratio to 10:6
-plot(x_range, y_values_2, 'k', 'LineWidth', lw);
-plot(beta_range, y_values_3, 'r', 'LineWidth', lw);
-plot(beta_range, y_values_4, 'r', 'LineWidth', lw);
-plot(beta_range_2, y_values_5, 'r', 'LineWidth', lw);
+% --- Define the Bandfunctions in the subwavelength regime ---
+    f_1 = @(x) sqrt(delta) * sqrt((1/s_1 + 1/s_2)     + sqrt(1/(s_1)^2     + 1/(s_2)^2 + 2/(s_1*s_2)*cos(x)));
+    f_2 = @(x) sqrt(delta) * sqrt((1/s_1 + 1/s_2)     - sqrt(1/(s_1)^2     + 1/(s_2)^2 + 2/(s_1*s_2)*cos(x)));
+    f_3 = @(x) sqrt(delta) * sqrt(abs((1/s_1 + 1/s_2) - sqrt(abs(1/(s_1)^2 + 1/(s_2)^2 - 2/(s_1*s_2)*cosh(x)))));
+    f_4 = @(x) sqrt(delta) * sqrt(abs((1/s_1 + 1/s_2) + sqrt(abs(1/(s_1)^2 + 1/(s_2)^2 - 2/(s_1*s_2)*cosh(x)))));
+    f_5 = @(x) sqrt(delta) * sqrt(abs((1/s_1 + 1/s_2) + sqrt(abs(1/(s_1)^2 + 1/(s_2)^2 + 2/(s_1*s_2)*cosh(x)))));
 
-y_val = f_1(pi);
-line([-4, 4], [y_val, y_val], 'Color', 'k', 'LineStyle', ':', 'LineWidth', 1);
-b_val = f_2(pi);
-line([-4, 4], [b_val, b_val], 'Color', 'k', 'LineStyle', ':', 'LineWidth', 1);
-g_val = f_1(0);
-line([-4, 4], [g_val, g_val], 'Color', 'k', 'LineStyle', ':', 'LineWidth', 1);
+% --- Define the range for x ---
+    x_range = -pi:0.08:pi;      % Plot real Bands over the first Brillouin zone
+    beta_bound = acosh((1/2) * (s_2^2 + s_1^2)/(s_1*s_2)); 
+    beta_range = -beta_bound:0.0098:beta_bound+0.001 ;
+    beta_range_2 = -4:0.08:4;   % Plot the complex bands over some range
 
-xlabel('\alpha and \beta respectively', 'FontSize', 18);
-ylabel('\omega^{\alpha, \beta}', 'FontSize', 18);
-legend('Band function', '', 'Gap function', '', '', 'Location', 'northeast', 'FontSize', 20);
-set(gca, 'FontSize', 22); % Adjust tick font size
-saveas(gcf, 'Band_1D_Dimer.pdf');
-hold off;
+% --- Evaluate the functions on the grid ---
+    y_values_1 = real(f_1(x_range));        % Real Band
+    y_values_2 = real(f_2(x_range));        % Real Band
+    y_values_3 = real(f_3(beta_range));     % Complex Band
+    y_values_4 = real(f_4(beta_range));     % Complex Band
+    y_values_5 = real(f_5(beta_range_2));   % Complex Band
+
+
+% --- Plotting the spectral Bands ---
+    figure;
+    lw = 4.5;   % Linewidth
+    fs = 18;    % Fontsize
+    
+    % Plotting of the Bands
+    plot(x_range, y_values_1, 'k', 'LineWidth', lw);
+    hold on;
+    pbaspect([8 6 1]); % Set the ratio to 10:6
+    plot(x_range, y_values_2     , 'k', 'LineWidth', lw);
+    plot(beta_range, y_values_3  , 'r', 'LineWidth', lw);
+    plot(beta_range, y_values_4  , 'r', 'LineWidth', lw);
+    plot(beta_range_2, y_values_5, 'r', 'LineWidth', lw);
+    
+    % Horizontal lines separating the Band and Bandgap
+    y_val = f_1(pi);
+    line([-4, 4], [y_val, y_val], 'Color', 'k', 'LineStyle', ':', 'LineWidth', 1);
+    b_val = f_2(pi);
+    line([-4, 4], [b_val, b_val], 'Color', 'k', 'LineStyle', ':', 'LineWidth', 1);
+    g_val = f_1(0);
+    line([-4, 4], [g_val, g_val], 'Color', 'k', 'LineStyle', ':', 'LineWidth', 1);
+    
+    % Adding labels, legend and ticks
+    xlabel('\alpha and \beta respectively', 'FontSize', fs);
+    ylabel('\omega^{\alpha, \beta}', 'FontSize', fs);
+    legend('Band function', '', 'Gap function', '', '', 'Location', 'northeast', 'FontSize', fs+2);
+    set(gca, 'FontSize', fs+4); 
+    saveas(gcf, 'Band_1D_Dimer.pdf');
+    hold off;
+
