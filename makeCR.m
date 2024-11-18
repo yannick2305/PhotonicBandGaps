c1a5346d5ccceb1df040f1af53ab2ddf398d884b
yannick2305 <yannick.debruijn23@gmail.com>
2024-11-18 15:11:52 +0100

    Update makeCR.m

--- makeCR.m
+++ makeCR.m
@@ -1,43 +1,59 @@
+%{
+    ---------------------------------------------------------
+    Author(s):    [Erik Orvehed HILTUNEN , Yannick DE BRUIJN]
+    Date:         [November 2024]
+    Description:  [Generate complex Capacitance matrix]
+    ---------------------------------------------------------
+%}
 
-function matC = makeCR(k, R, alpha, beta, L1x, L2, d_zeta, JHdata, JHijdata, N, N_multi, N_lattice)   
-        N = 1;
-        matS = makeS(k,R,alpha,L1x,L2,d_zeta,JHdata,JHijdata,N,N_multi,N_lattice);
-        matR = makeR(k, R, alpha, beta, N_multi, N_lattice);
-        matSR = (-matR + matS)  ;    % Single Layer potential for beta neq 0
-                                     % Minus sign because of the 2
-                                     % conventions used (Habib vs Erik)
+function matC = makeCR(k, R, alpha, beta, L1x, L2, d_zeta, JHdata, JHijdata, N, N_multi, N_lattice)  
+    %
+    % Description:
+    %   Function to compute the capacitance matrix using Single Layer Potential
+    %   and Multipole Expansion.
+    %
+    % Input Arguments:
+    %   k          - Wave number (scalar).
+    %   R          - Resonator radius (scalar).
+    %   alpha      - Real- quasimomentum, vector (R^2).
+    %   beta       - Complex part of the quasimomentum, vector (R^2).
+    %   L1x        - Lattice size in the x-direction (scalar).
+    %   L2         - Lattice size in the y-direction (scalar).
+    %   d_zeta     - Lattice spacing (scalar).
+    %   JHdata     - Data for JH matrix (structure or matrix).
+    %   JHijdata   - Data for JHij matrix (structure or matrix).
+    %   N=1        - Number resonators inside unit cell (scalar).
+    %   N_multi    - Multipole expansion range (scalar).
+    %   N_lattice  - Number of lattice points in Green's function (scalar).
+    %
+    % Output Arguments:
+    %   matC       - Capacitance matrix (scalar).
+ 
 
-        % represent in Fourier Basis ---------------------------------
-        n_range = -N_multi:N_multi;  % Define the range of the Fourier Basis
-        coefficients_P = zeros(size(n_range));
-        coefficients_M = zeros(size(n_range));
+    N = 1; % This code only works for one resonator inside the unit cell.
 
-        % Compute the Fourier coefficients numerically
-        %for i = 1:length(n_range)
-        %    n = n_range(i);
-        %    integrand = @(x) exp(R*beta(1)*cos(x) + R*beta(2)*sin(x)) .* exp(-1i*n*x);
-        %    coefficients_P(i) = 1/(2*pi) * integral(integrand, 0, 2*pi);
-        %end 
+    % -------- Generate the complex Single Layer Potential -----------
+    matS = makeS(k, R, alpha, L1x, L2, d_zeta, JHdata, JHijdata, N, N_multi, N_lattice);
+    matR = makeR(k, R, alpha, beta, N_multi, N_lattice);
+    matSR = (-matR + matS);  % Minus sign due to different conventions (Habib vs Erik)
 
-        %for i = 1:length(n_range)
-        %    n = n_range(i);
-        %    integrand = @(x) exp(-R*beta(1)*cos(x) - R*beta(2)*sin(x)) .* exp(-1i*n*x);
-        %    coefficients_M(i) = 1/(2*pi) * integral(integrand, 0, 2*pi);
-        %end 
+    % --- Multipole Expansion -----------------------------------------
+    n_range = -N_multi : N_multi;  % Fourier basis representation
 
-         % Compute the Fourier coefficients using modified Bessel functions
-        for i = 1:length(n_range)
-            n = n_range(i);
-            bI = besseli(n,R*norm(beta));
-            coefficients_P(i) = exp(-1i*n*atan2(beta(2),beta(1)))*bI;
-            coefficients_M(i) = exp(-1i*n*atan2(-beta(2),-beta(1)))*bI;
-        end 
-        %--------------------------------------------------------------
+    % --- Precompute constants ---
+    theta1 = atan2(beta(2), beta(1));
+    theta2 = atan2(-beta(2), -beta(1));
+    Rb_norm = R * norm(beta);
 
-        phi_j = coefficients_P; 
-        phi_j = transpose(phi_j);        
-        psi_j = matSR\phi_j;                                % "Inverting" matSR
-        matC = -2 * pi * R * dot(coefficients_M, psi_j);    % Compute capacitance matrix
+    % --- Vectorized Fourier coefficient computation ---
+    n = n_range(:);                                     % Ensure n_range is a column vector
+    bI = besseli(n, Rb_norm);                           % Compute Bessel functions for all n
+    coefficients_P = bI .* exp(-1i * n * theta1);       % Positive Fourier coefficients
+    coefficients_M = bI .* exp(-1i * n * theta2);       % Negative Fourier coefficients
+
+    % --- Compute scalar product to define the Capacitance matrix ---
+    phi_j = coefficients_P; 
+    psi_j = matSR \ phi_j;                              % Solve for psi_j by inverting matSR
+    matC = -2 * pi * R * dot(coefficients_M, psi_j);    % Final capacitance matrix computation
 
 end
-
