08dee1d4ed29b1c14fcae82f60fc6b7939523976
yannick2305 <yannick.debruijn23@gmail.com>
2024-12-16 11:17:55 +0100

    Update SkinEffectOneD.m

--- SkinEffectOneD.m
+++ SkinEffectOneD.m
@@ -10,92 +10,61 @@ close all;
 clear all;
 
 % --- Set the parameters ---
-    N      = 62;                    % Number of resonators (even number for dimers).
-    gamma  = 1;                     % Complex Gauge potential.
-    s      = mod(1:(N-1), 2) + 1;   % Resonator spacings (1, 2, 1, 2,..., 1, 2).
-    l      = mod(1:N    , 1) + 1;   % Length of the resonators.
+    N_cells = 20;          % Number of unit cells.
+    n       = 3;           % Number of resonators per unit cell.
+    spacing = [3, 2, 3];      % Spacings (length n).
+    length  = [1, 2, 1 ];      % Resonators (length n).
+    gamma   = 1;           % Complex Gauge potential.
+
+% --- Initialise arrays ---
+    s = repmat(spacing, 1, N_cells);  
+    l = repmat(length, 1, N_cells);   
+    N = N_cells * n;
+
 
 
 %%  --- Plot the eigenvector entries and illustrate exponential decay ---
 
-    plot_all_eigenvectors(N, s, gamma, l);
+    plot_all_eigenvectors(N, s, gamma, l, length);
 
 
 %% --- Plot the geometry of the resonator chain ---
 
-% --- Plot the resonators ---
-    % Initialize positions
-    x_start = 0;              
-    y_positions = 0;           
-
-    figure;
-    hold on;
-    for i = 1:N
-        x_end = x_start + l(i);
-        line([x_start, x_end], [y_positions, y_positions], 'Color', 'k', 'LineWidth', 4);
-    
-        % Update the starting position for the next resonator
-        if i < N
-            x_start = x_end + s(i);
-        end
-    end
-
-% --- Formatting the plot ---
-    set(gcf, 'Position', [100, 100, 800, 200]);
-    xlabel('x-position', 'Interpreter', 'latex', 'FontSize', 20);
-    set(gca, 'FontSize', 18-4);
-    set(gca, 'YTick', []);
-    axis equal;
-    grid off;
-    hold off;
+    PlotGeometry(N, l, s);
 
 
 %% --- Plot the eigenvalues ---
 
-% --- Generate the Capacitance matrix
-    capmat = generate_capmat(N, s, gamma, l);
-
-% --- Compute the eigenvalues of the capacitance matrix ---
-    [eigenvectors, eigenvalues_matrix] = eig(capmat);
-    eigenvalues = sort(diag(eigenvalues_matrix));
-
-% --- Plot the eigenvalues ---
-    figure;
-    plot(eigenvalues, 'bo', 'LineWidth', 2, 'MarkerSize', 8);  
-    xlabel('Index',      'Interpreter', 'latex', 'FontSize', 14);
-    ylabel('Eigenvalue', 'Interpreter', 'latex', 'FontSize', 14);
-    grid on;
+    PlotEigenvalues(N, s, gamma, l)
 
 
 %% --- Defining functions ---
 
 
-function plot_all_eigenvectors(N, s, gamma, l)
+function plot_all_eigenvectors(N, s, gamma, l, len)
 
     lw = 3;     % Thickness of the resonators
     fs = 24;    % Fontsize of the annotations
 
     % --- Generate the eigenvectors of the Capacitance matrix ---
-        capmat = generate_capmat(N, s, gamma, l);
+        capmat = Capacitance(N, s, gamma, l);
         [V, D] = eig(capmat);
         eigenvalues = diag(D);
         [~, sortIdx] = sort(eigenvalues);
-        sortedEigenvectors = V(:, sortIdx); % Reorder the eigenvectors
-
-        figure;
-        hold on;    
+        sortedEigenvectors = V(:, sortIdx);    
     
     % --- Plot the eigenvector entries ---
         figure;
         hold on;    
 
-        for i = 1:N
+        for i = 1:(N)
             plot(1:N, (abs(sortedEigenvectors(:, i))), '.', 'Color', 0.5 * [1, 1, 1], 'LineWidth', lw, 'MarkerSize', 15);
         end
 
     % --- Add the predicted exponential decay rate ---
         x = 0:N; 
-        y = exp(- gamma * 0.5 * 0.5 * (l(1)+l(2)) * x); 
+        constant = 1;
+        y =  constant * exp(- gamma * 0.5 * (1 / length(len)) * sum(len) * x); 
         plot(x, (y), 'r', 'LineWidth', lw); 
    
     % --- Formatting the plot ---
@@ -109,32 +78,79 @@ function plot_all_eigenvectors(N, s, gamma, l)
         hold off;
 end
 
-
-
-function capmat = generate_capmat(N, s, gamma, l)
+function capmat = Capacitance(N, s, gamma,  ell)
     
-    capmat = zeros(N, N);
-
-    % --- Populate the Capacitance matrix ---
+    capmat = zeros(N, N);  
+    
+    % --- Popolate the matrix ---
     for i = 1:N
         for j = 1:N
-            if i == 1 && j == 1
-                % Case 1: i = j = 1
-                capmat(i,j) = (gamma / s(i)) * (l(i) / (1 - exp(-gamma * l(i))));
-            elseif i == j && i > 1 && i < N
+            if i == j
+            % --- Populate  diagonal ---
+                if i == 1
+                    capmat(i,j) = (gamma / s(i)) * (ell(i) / (1 - exp(-gamma * ell(i))));
                 % Case 2: 1 < i = j < N
-                capmat(i,j) = (gamma / s(i)) * (l(i) / (1 - exp(-gamma * l(i)))) ...
-                    - (gamma / s(i-1)) * (l(i) / (1 - exp(gamma * l(i))));
-            elseif i == j - 1
-                % Case 3: i = j - 1
-                capmat(i,j) = - (gamma / s(i)) * (l(j) / (1 - exp(-gamma * l(j))));
-            elseif i == j + 1
-                % Case 4: i = j + 1
-                capmat(i,j) = (gamma / s(j)) * (l(i) / (1 - exp(gamma * l(j))));
-            elseif i == N && j == N
-                % Case 5: i = j = N
-                capmat(i,j) = - (gamma / s(N-1)) * (l(N) / (1 - exp(gamma * l(N))));
+                elseif i > 1 && i < N
+                    capmat(i,j) = (gamma / s(i))   * (ell(i) / (1 - exp(-gamma * ell(i)))) ...
+                                - (gamma / s(i-1)) * (ell(i) / (1 - exp( gamma * ell(i))));
+                % Case 3: i = j = N
+                else
+                    capmat(i,j) = -(gamma / s(i-1)) * (ell(i) / (1 - exp(gamma * ell(i))));
+                end
+
+            % --- Populate lower diagonal ---
+                elseif i == j - 1
+                    % Case 4: 1 <= i = j - 1 <= N - 1
+                    capmat(i,j) = -(gamma / s(i)) * (ell(i) / (1 - exp(-gamma * ell(j))));
+
+            % --- Populate upper diagonal ---
+                elseif i == j + 1 
+                    % Case 5: 2 <= i = j + 1 <= N
+                    capmat(i,j) = (gamma / s(j)) * (ell(i) / (1 - exp(gamma * ell(j))));
             end
         end
     end
 end
+
+function PlotGeometry(N, l, s)
+
+    x_start = 0;  % Initialize starting position of the first segment            
+
+    figure;
+    hold on;
+    for i = 1:N
+        x_end = x_start + l(i);
+        line([x_start, x_end], [0, 0], 'Color', 'k', 'LineWidth', 4);
+        if i < N
+            x_start = x_end + s(i);
+        end
+    end
+
+    % --- Formatting the plot ---
+    set(gcf, 'Position', [100, 100, 800, 200]);  
+    xlabel('x-position', 'Interpreter', 'latex', 'FontSize', 20);
+    set(gca, 'FontSize', 18-4);
+    set(gca, 'YTick', []); 
+    axis equal;  
+    grid off;  
+    hold off;
+end
+
+function PlotEigenvalues(N, s, gamma, l)
+    
+    % --- Generate the Capacitance matrix ---
+    capmat = Capacitance(N, s, gamma, l);
+    
+    % --- Compute the eigenvalues of the capacitance matrix ---
+    [~, eigenvalues_matrix] = eig(capmat);
+    eigenvalues = sort(diag(eigenvalues_matrix));  
+    
+    % --- Plot the eigenvalues ---
+    figure;
+    plot(eigenvalues, 'bo', 'LineWidth', 2, 'MarkerSize', 8);  
+    xlabel('Index', 'Interpreter', 'latex', 'FontSize', 14);
+    ylabel('Eigenvalue', 'Interpreter', 'latex', 'FontSize', 14);
+    grid on; 
+end
+
+
