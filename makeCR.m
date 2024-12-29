8303b5b2d6332cabe43e3b037de4bb0df8fe2e7e
yannick2305 <yannick.debruijn23@gmail.com>
2024-12-29 20:38:54 +0100

    Update makeCR.m

--- makeCR.m
+++ makeCR.m
@@ -33,20 +33,20 @@ function matC = makeCR(k, R, alpha, beta, L1x, L2, d_zeta, JHdata, JHijdata, N, 
     N = 1; % This code only works for one resonator inside the unit cell.
 
     % -------- Generate the complex Single Layer Potential -----------
-    matS = makeS(k, R, alpha, L1x, L2, d_zeta, JHdata, JHijdata, N, N_multi, N_lattice);
-    matR = makeR(k, R, alpha, beta, N_multi, N_lattice);
+    matS  = makeS(k, R, alpha, L1x, L2, d_zeta, JHdata, JHijdata, N, N_multi, N_lattice);
+    matR  = makeR(k, R, alpha, beta, N_multi, N_lattice);
     matSR = (-matR + matS);  % Minus sign due to different conventions (Habib vs Erik)
 
     % --- Multipole Expansion -----------------------------------------
     n_range = -N_multi : N_multi;  % Fourier basis representation
 
     % --- Precompute constants ---
-    theta1 = atan2(beta(2), beta(1));
-    theta2 = atan2(-beta(2), -beta(1));
+    theta1  = atan2(beta(2), beta(1));
+    theta2  = atan2(-beta(2), -beta(1));
     Rb_norm = R * norm(beta);
 
     % --- Vectorized Fourier coefficient computation ---
-    n = n_range(:);                                     % Ensure n_range is a column vector
+    n  = n_range(:);                                    % Ensure n_range is a column vector
     bI = besseli(n, Rb_norm);                           % Compute Bessel functions for all n
     coefficients_P = bI .* exp(-1i * n * theta1);       % Positive Fourier coefficients
     coefficients_M = bI .* exp(-1i * n * theta2);       % Negative Fourier coefficients
@@ -54,6 +54,6 @@ function matC = makeCR(k, R, alpha, beta, L1x, L2, d_zeta, JHdata, JHijdata, N, 
     % --- Compute scalar product to define the Capacitance matrix ---
     phi_j = coefficients_P; 
     psi_j = matSR \ phi_j;                              % Solve for psi_j by inverting matSR
-    matC = -2 * pi * R * dot(coefficients_M, psi_j);    % Final capacitance matrix computation
+    matC  = -2 * pi * R * dot(coefficients_M, psi_j);   % Final capacitance matrix computation
 
 end
