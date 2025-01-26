580c8fe22477675d15f85b5b9a31f7de8c3b5589
yannick2305 <yannick.debruijn23@gmail.com>
2025-01-26 10:07:50 +0100

    Update makeRSlow.m

--- makeRSlow.m
+++ makeRSlow.m
@@ -6,13 +6,27 @@
     ----------------------------------------------------------------------
 %}
 
-function SLP = makeRSlow(k0, R, alpha, beta, N_SLP, N_lattice)
-    rho_0 = 1000;
-    kappa_0 = 1000;
-    v0 = sqrt(kappa_0/rho_0);
-    w = k0 / v0;                % rescale from k and w (Here k = w)
-    SLP = SLP_R(w, N_lattice, N_SLP, alpha, beta, R);
-    SLP = SLP_R(k0, N_lattice, N_SLP, alpha, beta, R);
+% Objective: Straightforward implmentation from the formula, though it is
+%            not he most efficient implmentation.
+% -> makeR.m is an efficient implmentation of this code.
+
+% Perform the boundary integral for SLP, returns of form Mat(2N+1)
+function SLP_matrix = makeRSlow(w, R, alpha, beta, N_SLP, N_lattice)
+                   
+    SLP_size = 2 * N_SLP + 1;
+    SLP_matrix = zeros(SLP_size , SLP_size);           
+
+    m_range = - N_SLP:N_SLP;       
+    n_range = - N_SLP:N_SLP;
+    
+    % Compute entries of the SLP matrix
+    for i = 1:SLP_size              % SLP_size = 2 * N_SLP + 1
+        m = m_range(i);             % i.e. m = {-N_SLP,...,0,...,N_SLP}
+        for j = 1:SLP_size
+            n = n_range(j);
+            SLP_matrix(i, j) = lattice_sum(m, n, w, N_lattice, alpha, beta, R);
+        end
+    end
 end
 
 % Generates part of the complex Green's function
@@ -32,13 +46,13 @@ function lattice_sum_value = lattice_sum(m, n, w, n_lat, alpha, beta, R)
     a = [2*pi, 0]; % Initialize the dual lattice
     b = [0, 2*pi];
     lattice_sum_value = 0;
+
     for i = -n_lat:n_lat
         for j = -n_lat:n_lat
             q = i * a + j * b;
             alpha_q = alpha + q;
-            psi = atan2(alpha_q(1), alpha_q(2));
             psi = atan2(alpha_q(2), alpha_q(1));
-            scale = 2*pi*R * exp(1i * psi * (n - m)) * 1i^(m - n) * (-1)^n;
+            scale  = 2*pi*R * exp(1i * psi * (n - m)) * 1i^(m + n) * (-1)^n;
             arg = R*sqrt(alpha_q(1)^2 + alpha_q(2)^2);
             res = scale * besselj(m, arg) * besselj(n, arg)* Green(q, w, alpha, beta);
 
@@ -46,25 +60,6 @@ function lattice_sum_value = lattice_sum(m, n, w, n_lat, alpha, beta, R)
 
         end
     end    
-    lattice_sum_value = 1/(1) * lattice_sum_value; %rescale to volume of the unit cell, (|Y| = 1)
+    lattice_sum_value = 1/(1) * lattice_sum_value; % Rescale to volume of the unit cell, (|Y| = 1)
 end
 
-% Perform the boundary integral for SLP, returns of form Mat(2N+1)
-function SLP_matrix = SLP_R(w, N_lattice, N_SLP, alpha, beta, R)
-    SLP_size = 2 * N_SLP + 1;
-
-    SLP_matrix = zeros(SLP_size , SLP_size);            % Initialize matrix
-
-    % Compute entries of the SLP matrix
-    m_range = - N_SLP:N_SLP;        % Finite Fourier Basis
-    n_range = - N_SLP:N_SLP;
-
-    for i = 1:SLP_size              % SLP_size = 2 * N_SLP + 1
-        m = m_range(i);             % i.e. m = {-N_SLP,...,0,...,N_SLP}
-        for j = 1:SLP_size
-            n = n_range(j);
-            SLP_matrix(i, j) = lattice_sum(m, n, w, N_lattice, alpha, beta, R);
-       end
-    end
-
-end
